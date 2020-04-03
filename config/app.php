83461b0dea6e6ed0990fcd99ceee22b7765163f3
Jael Ghiso <43589070+jaelghiso@users.noreply.github.com>
2020-04-03 07:07:12 -0300

    fix

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', $_ENV['APP_KEY']),
+    'key' => env('APP_KEY', base64_decode('TXFi6v1LCsvpMb6G19tnhygNaLOOSs4y08tDXjsA4yg=')),
 
     'cipher' => 'AES-128-CBC',
 
